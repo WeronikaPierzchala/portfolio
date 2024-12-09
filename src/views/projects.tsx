72c097404fcad63e0236e37a336b8dfd21a0dc29
Weronika Pierzchała <weronikajarczyk2.0@gmail.com>
2024-12-09 16:37:32 +0100

    Create Projects page

---
+++ src/views/projects.tsx
@@ -0,0 +1,99 @@
+import { LineGradient } from "../components";
+import { motion } from "framer-motion";
+import project1 from "../assets/project-1.jpeg";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+export const Projects = () => {
+  console.log(Array(7));
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
+          purus nec eget eleifend ut elit.
+        </p>
+      </motion.div>
+
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            BEAUTIFUL USER INTERFACES
+          </div>
+          {[1, 2, 3, 4, 5, 6, 7].map((_, index) => {
+            return <Project title="Project 1" key={index} />;
+          })}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+type ProjectProps = {
+  title: string;
+};
+
+const Project = ({ title }: ProjectProps) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+          porttitor accumsan tincidunt.
+        </p>
+      </div>
+      <img src={project1} alt={projectTitle} />
+    </motion.div>
+  );
+};
