e4632a73a262c385a9cf434118bc64a56b102c36
Weronika Pierzchała <weronikajarczyk2.0@gmail.com>
2024-12-09 11:02:36 +0100

    Use new components in the /app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useMediaQuery } from "./hooks";
-import { PAGES, PageType } from "./const"
+import { Navbar } from "./components/navbar";
+import { DotGroup } from "./components/dot-group";
+import { PageType } from "./const";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<PageType>(PAGES.home);
+  const [selectedPage, setSelectedPage] = useState<PageType>("home");
+  const [isPageTop, setIsPageTop] = useState<boolean>(true);
   const isTablet = useMediaQuery("md");
 
-  return <div className="app bg-deep-blue">
-    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-  </div>;
+  useEffect(() => {
+    const checkIfPageTop = () => setIsPageTop(window.scrollY === 0);
+    document.addEventListener("scroll", checkIfPageTop);
+    return () => document.addEventListener("scroll", checkIfPageTop);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        backgroundColor={isPageTop ? "" : "bg-red"}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {!isTablet && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
