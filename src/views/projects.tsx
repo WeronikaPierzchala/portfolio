bb585d9d8bc5585b45a5ac824185acd5e0530d90
Weronika Pierzchała <weronikajarczyk2.0@gmail.com>
2024-12-10 15:12:10 +0100

    Adjust projects page

--- src/views/projects.tsx
+++ src/views/projects.tsx
@@ -1,4 +1,3 @@
-import { LineGradient } from "../components";
 import { motion } from "framer-motion";
 import project1 from "../assets/project-1.jpeg";
 
@@ -25,44 +24,40 @@ export const Projects = () => {
           visible: { opacity: 1, y: 0 },
         }}
       >
-        <div>
-          <p className="font-playfair font-semibold text-4xl">
-            <span className="text-red">PRO</span>JECTS
-          </p>
-          <div className="flex justify-center mt-5">
-            <LineGradient width="w-2/3" />
-          </div>
-        </div>
-        <p className="mt-10 mb-10">
-          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
-          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
-          purus nec eget eleifend ut elit.
+        <p className="font-playfair font-semibold text-4xl">
+          <span className="text-red">PRO</span>JECTS
         </p>
+
+        <p className="mt-10 mb-10">I was a part of</p>
       </motion.div>
 
       <div className="flex justify-center">
         <motion.div
-          className="sm:grid sm:grid-cols-3"
+          className="sm:grid md:grid-cols-3 w-full"
           variants={container}
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
         >
-          <div
-            className="flex justify-center text-center items-center p-10 bg-red
-              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
-          >
-            BEAUTIFUL USER INTERFACES
-          </div>
-          {[1].map((_, index) => {
-            return <Project title="Project 1" key={index} />;
-          })}
-          <div
-            className="flex justify-center text-center items-center p-10 bg-blue
-              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
-          >
-            SMOOTH USER EXPERIENCE
-          </div>
+          <Project
+            title="Braintrust"
+            description="Braintrust is a user-owned talent network that connects skilled freelancers with top companies. It operates on a decentralized model, allowing professionals to retain more of their earnings while clients access high-quality talent at competitive rates. Built on blockchain technology, Braintrust ensures transparency, fairness, and a more efficient way to match talent with opportunities."
+            bgColor="bg-red"
+            link="https://www.usebraintrust.com/"
+          />
+          <Project
+            title="Braintrust Dashboard"
+            description="The Braintrust Network Dashboard provides an overview of key metrics and activities within the Braintrust ecosystem. It includes data on active talent, job postings, client engagement, network growth, and transaction statistics. Users can track their contributions, earnings, and overall performance, gaining insights into the network's health and opportunities for collaboration."
+            bgColor="bg-white"
+            color="text-primary"
+            link="https://info.app.usebraintrust.com/"
+          />
+          <Project
+            title="Other"
+            description="If you are interested in viewing my other projects, please click this tile to view the source code on GitHub"
+            bgColor="bg-secondary"
+            link="https://github.com/WeronikaPierzchala/"
+          />
         </motion.div>
       </div>
     </section>
@@ -76,23 +71,34 @@ const projectVariant = {
 
 type ProjectProps = {
   title: string;
+  description: string;
+  bgColor: string;
+  link: string;
+  color?: string;
 };
 
-const Project = ({ title }: ProjectProps) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
-  const projectTitle = title.split(" ").join("-").toLowerCase();
+const Project = ({
+  title,
+  description,
+  bgColor,
+  link,
+  color,
+}: ProjectProps) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-100 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-primary`;
 
   return (
-    <motion.div variants={projectVariant} className="relative">
-      <div className={overlayStyles}>
-        <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
-        </p>
-      </div>
-      <img src={project1} alt={projectTitle} />
-    </motion.div>
+    <a href={link} target="_blank" rel="noreferrer">
+      <motion.div
+        variants={projectVariant}
+        className={`relative h-[400px] w-full max-w-100 ${bgColor} flex justify-center items-center`}
+      >
+        <p className={`text-2xl font-playfair ${color}`}>{title}</p>
+        <div className={overlayStyles}>
+          <p className="text-2xl font-playfair">{title}</p>
+          <p className="mt-7">{description}</p>
+        </div>
+      </motion.div>
+    </a>
   );
 };
