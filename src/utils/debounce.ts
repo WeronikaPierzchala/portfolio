9766538bffa1e2ab901701713d25d6cc0fada658
Weronika Pierzchała <weronikajarczyk2.0@gmail.com>
2024-12-11 15:39:23 +0100

    Delete unused utils

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-export const debounce = (callback: () => void, wait = 100) => {
-  let timeoutId: number | undefined = undefined;
-  return () => {
-    window.clearTimeout(timeoutId);
-    timeoutId = window.setTimeout(() => {
-      callback();
-    }, wait);
-  };
-};
--- src/utils/element-visible.ts
+++
@@ -1,6 +0,0 @@
-export const isElementVisible = (element: HTMLElement | null) => {
-  if (!element) return false;
-
-  const item = element.getBoundingClientRect();
-  return item.top + item.height >= window.innerHeight && item.top - 190 < 0;
-};
